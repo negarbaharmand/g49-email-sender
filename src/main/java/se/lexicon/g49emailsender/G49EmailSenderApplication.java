40bd20d8cfbe210e5e00391488c484d49a208c58
Negar Baharmand <negarbaharmand@outlook.com>
2024-06-24 09:09:23 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g49emailsender/G49EmailSenderApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g49emailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G49EmailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G49EmailSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g49emailsender/G49EmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g49emailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G49EmailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
